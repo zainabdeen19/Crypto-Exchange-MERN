dc28ee85be165c997e50c5415192d93b5c6a55e2
Zain Abdeen <zainabiddeen19@gmail.com>
2022-06-25 00:00:00 -0400

    Commit: loginRoute.js

---
+++ api/routes/loginRoute.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const loginController = require("../controllers/loginController");
+
+router.post("/", loginController.login);
+
+module.exports = router;
+ 
