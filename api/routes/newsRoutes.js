6f227c8752dc5065fa2a1ba8f34a14d1a46c2f1b
Zain Abdeen <zainabiddeen19@gmail.com>
2022-07-15 00:00:00 -0400

    Commit: newsRoutes.js

---
+++ api/routes/newsRoutes.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const newsController = require("../controllers/newsController");
+
+router.get("/", newsController.getNews);
+
+module.exports = router;
+ 
