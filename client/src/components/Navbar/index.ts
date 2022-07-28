1f5af978fcf4a281af0fa1d81e05a4d04c950c68
Zain Abdeen <zainabiddeen19@gmail.com>
2022-07-28 00:00:00 -0400

    Commit: index.ts

---
+++ client/src/components/Navbar/index.ts
@@ -0,0 +1,7 @@
+export { CryptoWallet } from "./Profile/CryptoWallet";
+export { Profile } from "./Profile/Profile";
+export { SellCryptoModal } from "./Profile/SellCryptoModal";
+export { WalletRow } from "./Profile/WalletRow";
+
+export { Navbar } from "./Navbar";
+ 
