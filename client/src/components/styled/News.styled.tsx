365388e28c4c5a5c91e0089fa8f38d44ee3a2b45
Zain Abdeen <zainabiddeen19@gmail.com>
2022-08-25 00:00:00 -0400

    Commit: News.styled.tsx

---
+++ client/src/components/styled/News.styled.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const NewsArticleWrapper = styled.div`
+  min-height: calc(100vh - 60px);
+  padding-bottom: 15px;
+  background-color: ${(props) => (props.theme === "light" ? "white" : "black")};
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  .newsArticlesContainer {
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+`;
+ 
