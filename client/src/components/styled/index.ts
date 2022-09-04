f172ddaa4682c8aed339f9f5b532286b71801f0c
Zain Abdeen <zainabiddeen19@gmail.com>
2022-09-04 00:00:00 -0400

    Commit: index.ts

---
+++ client/src/components/styled/index.ts
@@ -0,0 +1,19 @@
+export {
+  SellCryptoFormWrapper,
+  PurchaseCryptoFormWrapper,
+} from "./Forms.styled";
+export {
+  HomeWrapper,
+  NewsArticlesWrapper,
+  CryptosWrapper,
+  CryptoCardWrapper,
+} from "./Home.styled";
+export { LoginWrapper } from "./Login.styled";
+export { NavbarWrapper, ProfileDrawerWrapper } from "./Navbar.styled";
+export { NewsArticleWrapper } from "./News.styled";
+export {
+  FeaturesWrapper,
+  IntroCardWrapper,
+  WelcomePageWrapper,
+} from "./Welcome.styled";
+ 
